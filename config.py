87fc7222b3a701fdf45bd24b1737be8e5fcad0b7
Vladislav.Abramov <vladislav.abramov@easycomm.ru>
2025-05-11 00:00:22 +0100

    data-collection-flow

--- config.py
+++ config.py
@@ -10,3 +10,8 @@ DB_NAME = os.environ["POSTGRES_DB"]
 USERNAME = os.environ["USERNAME"]
 PASSWORD = os.environ["PASSWORD"]
 DB_URL = f'postgresql://{USERNAME}:{PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+
+## APIs
+FMP_API_KEY = os.environ["FMP_API_KEY"]
+FMP_SP500_CONSTITUENT_URL = f'https://financialmodelingprep.com/api/v3/historical/sp500_constituent?apikey={FMP_API_KEY}'
